ee1fa0502eb0a3f22392f5af725e0564a65c028f
Marian-Ferenc2 <Marian.Ferenc2@ibm.com>
2021-06-27 16:55:13 +0200

    feat: pulling namespaces and ingresses

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,43 @@
 <template>
-  <hello-world />
+  <v-container fluid>
+    <span v-for="(namespace, index) in namespaceList" v-bind:key="index">
+      <h2>{{ namespace.name }} namespace</h2>
+      <v-divider></v-divider>
+      <span
+        v-for="(ingress, index) in namespace.ingressList"
+        v-bind:key="index"
+      >
+        <a>https://{{ ingress.host }}</a>
+      </span>
+    </span>
+  </v-container>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import axios from "axios";
 
 export default Vue.extend({
   name: "Home",
+  data: () => ({
+    namespaceList: [],
+    ingressesList: [],
+  }),
+  async mounted() {
+    try {
+      const { data } = await axios.get("http://127.0.0.1:3000/api/namespaces");
+      data.forEach(async (element) => {
+        if (element.name != "") {
+          const { data } = await axios.get(
+            "http://127.0.0.1:3000/api/ingresses?namespace=" + element.name
+          );
 
-  components: {
-    HelloWorld,
+          this.namespaceList.push({ name: element.name, ingressList: data });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
   },
 });
 </script>
